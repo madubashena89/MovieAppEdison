0c6effb07134dd1f8b82b599682823ace8c213aa
madubashena89 <madubashena89@gmail.com>
2020-08-22 19:12:39 -0500

    db test class added

---
+++ app/src/main/java/top/stores/movieappedison/roomDB/AppDataBase.kt
@@ -0,0 +1,28 @@
+package top.stores.movieappedison.roomDB
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [MovieEntity::class], version = 1)
+abstract class AppDataBase : RoomDatabase(){
+    abstract fun MovieDao(): MovieDao
+
+
+    companion object {
+        @Volatile
+        private var instance: AppDataBase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: buildDatabase(context).also { instance = it }
+        }
+
+        private fun buildDatabase(context: Context) = Room.databaseBuilder(
+            context,
+            AppDataBase::class.java, "movie_list.db"
+        )
+            .build()
+    }
+}
